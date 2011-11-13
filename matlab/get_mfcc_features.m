ddb7f63bcd7f1134d93708846bce1e89232bc617
Yang Hong <yangster@stanford.edu>
2011-11-12 21:01:36 -0800

    added step_time param to mfcc.m, added get_mfcc_features.m

---
+++ matlab/get_mfcc_features.m
@@ -0,0 +1,31 @@
+function mfcc_cells = get_mfcc_features(CSV_FILE_NAME, NUM_BINS, NUM_FRAMES, NUM_COEFF, STEP_TIME)
+% MFCC_CELLS = GET_MFCC_FEATURES(CSV_FILE_NAME, NUM_BINS, NUM_FRAMES,
+%              NUM_COEFF, STEP_TIME);
+%
+% MFCC_CELLS is a <# songs> by 2 cell matrix, where column 1 is the means
+% of the mfcc and column 2 is the covariances of the mfcc. Each row is the
+% mfcc data of a song.
+%
+% Heuristics:
+%   NUM_BINS = 20; NUM_COEFF = 15; STEP_TIME = 0.010;
+%
+% See also: mfcc.m, mp3tags_to_csv.py
+%
+
+fid = fopen(CSV_FILE_NAME);
+csv_data = textscan(fid, '%s %s %s %s', 'Delimiter', ',', 'CollectOutput', 1);
+csv_data = csv_data{1};
+fclose(fid);
+
+numRows = size(csv_data, 1);
+mfcc_cells = cell(numRows, 2);
+
+for row = 1:numRows
+    cur_song = csv_data(row, :);
+    song_file_name = cur_song(1, 1);
+    mfcc_mat = mfcc(song_file_name, NUM_BINS, NUM_FRAMES, NUM_COEFF, STEP_TIME);  
+    mfcc_cells{row, 1} = mean(mfcc_mat);
+    mfcc_cells{row, 2} = cov(mfcc_mat);
+end
+
+end
--- matlab/mfcc.m
+++ matlab/mfcc.m
@@ -1,10 +1,12 @@
-function MFCC_matrix = mfcc(filename, nframes, nbins)
-% MFCC_MATRIX = MFCC(FILENAME, NFRAMES, NBINS)
+function MFCC_matrix = mfcc(filename, nbins, nframes, ncoeff, step_time)
+% MFCC_MATRIX = MFCC(FILENAME, NBINS, NFRAMES, NCOEFF, STEP_TIME)
 %
 % FILENAME is a string specifying a .mp3 file.
-% MFCC_MATRIX is the nframes-by-nbins matrix of MFCC coefficients.
+% MFCC_MATRIX is the nframes-by-ncoeff matrix of MFCC coefficients.
 % NFRAMES is how many frames (20 ms samples) of the song to use.
 % NBINS is how many mel frequency bins to map each frame spectrum to.
+% NCOEFF is how many mel coefficients to keep (NCOEFF <= NBINS).
+% STEP_TIME is how often (in seconds) to capture a 20ms frame.
 %
 %      - Mel-Frequency Ceptrum Coefficients -
 % 1. Take the Fourier Transform of a 20-30ms windowed excerpt of a signal.
@@ -16,18 +18,23 @@ function MFCC_matrix = mfcc(filename, nframes, nbins)
 % 5. The MFCCs are the amplitudes of the resulting spectrum.
 %
 % Heuristics:
+%    nframes = 200;       % from IMECS 2010 paper (Li, Chan, Chun)
 %    nbins = 20;          % 20 mel freq bins
+%    ncoeff = 15;         % keep 15 of the 20 coefficients
 %    frame_time = 0.020;  % 20ms frames
+%    step_time = 0.030;   % take a 20ms frame every 30ms (10ms gap)
+%    OR
+%    step_time = 0.010;   % take a 20ms frame every 10ms (50% overlap)
 %
 % Example:
-% nframes = 1000; nbins = 20;
+% nframes = 200; nbins = 20; ncoeff = 15; step_time = 0.010;
 % filename = 'songname.mp3';
-% MFCC_matrix = mfcc(filename, nframes, nbins);
+% MFCC_matrix = mfcc(filename, nframes, nbins, ncoeff, step_time);
 %
 % Notes:
 % - Can also support .wav or .au audio file.
 % - For 30 sec audio file, analysis time of ~0.7 sec, given nframes=1000,
-% nbins=20.
+% nbins=20, ncoeff=20, step_time=0.030.
 %
 % See also: melbankm.m, mp3read.m
 %
@@ -50,11 +57,10 @@ function MFCC_matrix = mfcc(filename, nframes, nbins)
 
 % nframes = 1000;
 % nbins = 20;                 % # mel filters (bins)
-% song_len = length(s);       % length of song
 fft_len = 512;              % length of fft
 frame_time = 0.020;         % # seconds/frame
 frame_len = floor(fs*frame_time);  % # samples/frame
-step_time = 0.010;          % # seconds/frame step
+% step_time = 0.010;          % # seconds/frame step
 step_len = floor(fs*step_time);    % # samples/frame step
 
 MFCC_matrix = zeros(nframes, nbins);
@@ -63,8 +69,8 @@ window = hamming(frame_len);
 stop = 1+floor(fft_len/2);  % 257
 
 for i = 1:nframes
-    first = (i-1)*(frame_len+step_len)+1;
-    last = (i-1)*(frame_len+step_len)+frame_len;
+    first = (i-1)*step_len + 1;
+    last = (i-1)*step_len + frame_len;
     f = s(first:last);
     f = f.*window;
     f = fft(f, frame_len);
@@ -72,6 +78,7 @@ for i = 1:nframes
     f = abs(f(1:stop)).^2;
     m = log10(mel_bins*f);
     m = dct(m);          % dim(m): nbins x 1
+    m = m(1:ncoeff);
     MFCC_matrix(i, :) = m';
 end
 
