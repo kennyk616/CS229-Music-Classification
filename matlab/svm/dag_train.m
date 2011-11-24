6c46531a56a10ef71d8143369341e4db35bf6b5f
Mickey H <m.haggblade@gmail.com>
2011-11-24 08:14:14 -0800

    First iteration of dag_train.

---
+++ matlab/svm/dag_train.m
@@ -0,0 +1,19 @@
+% vim: set tabstop=4 shiftwidth=4 :
+function [ dag_model ] = dag_train( training_data )
+%DAG_TRAIN trains a multi-class DAG SVM model.
+%   training_data: A cell vector where each cell is an m by n matrix of m
+%       training points each made up of n features.
+num_categories = length(training_data);
+%m is a vector whose i'th position holds the number of training samples in
+%the i'th cell of training data.
+m = zeros(1, num_categories);
+for i = 1:num_categories
+    m(i) = size(training_data(i), 1);
+%populate dag_model, a cell matrix of liblinear models.
+dag_model = cell(num_categories);
+for i = 1:num_categories - 1
+    for j = i:num_categories
+        train_category = [-1*ones(m(i), 1); ones(m(j), 1)];
+        sparse_train_matrix = sparse([training_data{i}; training_data{j}]);
+        dag_model{i, j} = train(train_category, sparse_train_matrix);
+end
