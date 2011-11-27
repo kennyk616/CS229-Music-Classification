987429636c0586b48be1ca8c67b9f807da217524
Kenny Kao <kennyk616@gmail.com>
2011-11-27 15:51:27 -0800

    remove mp3, standardize training and testing .mat

--- matlab/mfcc_to_data.m
+++ matlab/mfcc_to_data.m
@@ -1,12 +1,20 @@
 %test
 clear all;
 
-CSV_FILE_PATH = GetFullPath('../py/output.csv');
+%training data
+CSV_FILE_PATH_training = GetFullPath('../py/train400.csv');
+CSV_FILE_PATH_testing = GetFullPath('../py/test400.csv');
 NUM_BINS = 22;
 NUM_FRAMES = 200;
 NUM_COEFF = 15;
 STEP_TIME = 0.02;
 
-mfcc_cells = get_mfcc_features(CSV_FILE_PATH, NUM_BINS, NUM_FRAMES, NUM_COEFF, STEP_TIME);
-save mfcc_test_data mfcc_cells;
+mfcc_cells_training400 = get_mfcc_features(CSV_FILE_PATH_training, NUM_BINS, NUM_FRAMES, NUM_COEFF, STEP_TIME);
+save mfcc_training_data400 mfcc_cells_training400;
+
+mfcc_cells_testing400 = get_mfcc_features(CSV_FILE_PATH_testing, NUM_BINS, NUM_FRAMES, NUM_COEFF, STEP_TIME);
+save mfcc_testing_data400 mfcc_cells_testing400;
+
+
+
 
