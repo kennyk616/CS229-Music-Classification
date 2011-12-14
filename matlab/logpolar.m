92da35f6605e2478e8e6d0b725077e15154df79c
Yang Hong <yangster@stanford.edu>
2011-12-13 21:43:52 -0800

    working and improved log-polar transform for 2D mellin

--- matlab/logpolar.m
+++ matlab/logpolar.m
@@ -3,26 +3,24 @@ function P = logpolar(I)
 %
 % The log-polar coordinate space is used by the 2D Mellin transform
 % in image feature extraction. The interpolation method used
-% is bilinear interpolation.
+% is bilinear interpolation. The radius of the inscribed circle is taken.
+%
+% Note: currently hardcoded to 64 x 64 images for speed, easily generalized
 %
 % See also: mellin2d.m, mellin_test.m
 %
 
 [nrows, ncols] = size(I);
-P = zeros(nrows, ncols);
-theta_step = 2*pi/ncols;  % how much to increment theta
-base = 10^(log10(nrows)/nrows);  % log-polar base
-halfrows = nrows/2; halfcols = ncols/2;
-for i = 1:nrows
-    theta = theta_step;
-	for j = 1:ncols
-		r = base^i-1;
-		theta = theta + theta_step;
-		x = round(r*cos(theta)+halfcols);
-		y = round(r*sin(theta)+halfrows);
-        if ((x>0) && (y>0) && (x<ncols) && (y<nrows))
-            P(i,j) = I(y,x);
-        end
-	end
-end
+% theta_step = 2*pi/nrows;
+theta_step = 0.099;  % hardcoded for 64x64 P
+% rho_max = ceil(2*pi/theta_step);
+rho_max = 64;
+center = [nrows/2 ncols/2];
+d = min([ncols-center(1) center(1)-1 nrows-center(2) center(2)-1]);
+
+Theta = 0:theta_step:2*pi;
+Rho = logspace(0, log10(d), rho_max)';
+X = round(Rho*cos(Theta) + center(1));
+Y = round(Rho*sin(Theta) + center(2));
+P = interp2(I, X, Y, 'linear');
 
