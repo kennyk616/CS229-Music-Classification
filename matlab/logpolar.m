020e7038024f6ed0549bdb79db7cdcddded1a35a
Yang Hong <yangster@stanford.edu>
2011-12-16 16:39:59 -0800

    added working mellin 2d transform for image feature extraction, calls logpolar and mellinbins functions

--- matlab/logpolar.m
+++ matlab/logpolar.m
@@ -5,21 +5,32 @@ function P = logpolar(I)
 % in image feature extraction. The interpolation method used
 % is bilinear interpolation. The radius of the inscribed circle is taken.
 %
-% Note: currently hardcoded to 64 x 64 images for speed, easily generalized
+% I and P are both square matrices of the same size.
 %
-% See also: mellin2d.m, mellin_test.m
+% Note: consider hardcoding (ie to 64x64) for additional speed
+%
+% See also: mellin2d.m, mellin_test.m, interp2.m
 %
 
-[nrows, ncols] = size(I);
-% theta_step = 2*pi/nrows;
-theta_step = 0.099;  % hardcoded for 64x64 P
-% rho_max = ceil(2*pi/theta_step);
-rho_max = 64;
+[nrows, ncols] = size(I);  % should be same (I is square)
+theta_step = 2*pi/nrows;
+% theta_step = 0.099;  % hardcoded for 64x64 P
+rho_max = ceil(2*pi/theta_step);
+% rho_max = 64;  % hardcoded for 64x64 P
 center = [nrows/2 ncols/2];
 d = min([ncols-center(1) center(1)-1 nrows-center(2) center(2)-1]);
 
-Theta = 0:theta_step:2*pi;
+Theta = 0:theta_step:2*pi;  
+tlen = length(Theta);
 Rho = logspace(0, log10(d), rho_max)';
+rlen = length(Rho);
+% ensure same size (possibly 1 off due to rounding error)
+if (tlen ~= rlen)
+    if (tlen > nrows)
+        Theta = Theta(1:end-1);
+    else Rho = Rho(1:end-1);
+    end
+end
 X = round(Rho*cos(Theta) + center(1));
 Y = round(Rho*sin(Theta) + center(2));
 P = interp2(I, X, Y, 'linear');
